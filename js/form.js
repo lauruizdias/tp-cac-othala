d38f6ca3bb78cdd1dac4eccb876ac307252e03cf
Laura Ruiz Dias <103478165+lauruizdias@users.noreply.github.com>
2023-11-05 22:23:38 -0300

    Cambios

---
+++ js/form.js
@@ -0,0 +1,22 @@
+function validarFormulario() {
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var tel = document.getElementById("tel").value;
+
+    var mensaje = document.getElementById("mensaje").value;
+
+    // Validación de campos vacíos
+    if (nombre === "" || email === "" || tel === ""|| mensaje === "") {
+        alert("Todos los campos son obligatorios.");js/form.js
+        return false;
+    }
+
+    // Validación de formato de email electrónico
+    var emailRegExp = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if (!email.match(emailRegExp)) {
+        alert("El email electrónico no es válido.");
+        return false;
+    }
+
+    return true;
+}
